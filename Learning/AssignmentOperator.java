bb03bb1ab0212d5edf2611e9628772186a16636c
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2021-02-15 00:04:56 +0530

    random learning

--- Learning/AssignmentOperator.java
+++ Learning/AssignmentOperator.java
@@ -1,10 +1,12 @@
 public class AssignmentOperator {
     public static void main(String[] args){
-        System.out.println(10+5);
-        int x=5;
-        System.out.println(x<3&&x>4);
-        System.out.println(x<4||x>2);
-        int y=x+33;
-        System.out.println(y);
+    char a=56;
+
+    int b=20;
+
+    int c=a+b;
+
+    System.out.println(c);
+        
     }
 }
